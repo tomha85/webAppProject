7d199d290e55fc80f8e987b3e7824e3d0243772f
Tom Ha <31414852+tomha85@users.noreply.github.com>
2022-04-24 22:12:57 -0400

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,18 +7,16 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-import sys
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-handler = logging.StreamHandler(stream=sys.stdout)
-handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
-app.logger.info('Article CMS')
 
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+
+import FlaskWebProject.views
